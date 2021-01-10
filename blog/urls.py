a28d233f47c1748493de3eead2f7775665180b70
alirezaatabaki <alirezaatabaki@icloud.com>
2021-01-10 10:42:55 +0330

    ADD API views to project

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,10 @@ app_name = "blog"
 urlpatterns = [
     path('', views.IndexPage.as_view(), name='index'),
     path('contact/', views.ContactPage.as_view(), name='contact'),
+    path('article/all/', views.AllArticleAPIView.as_view(), name='all_articles'),
+    path('article/<str:article>', views.SingleArticleAPIView.as_view(), name='single_article'),
+    path('article/search/<str:query>', views.SearchArticleAPIView.as_view(), name='search_article'),
+    path('^article/submit/', views.SubmitArticleAPIView.as_view(), name='submit_article'),
+    path('^article/update-cover/', views.UpdateArticleAPIView.as_view(), name='update_article'),
+    path('^article/delete/', views.DeleteArticleAPIView.as_view(), name='delete_article'),
 ]
